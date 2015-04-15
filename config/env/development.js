0d1988c463ccd57559df197066e6bfb8945d7857
Ícaro Harry <icaropc17@gmail.com>
2015-04-15 15:35:34 -0300

    facebook oauth strategy implemented

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
     db: 'mongodb://localhost/mean-book',
-    sessionSecret: 'developmentSessionSecret'
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: '826355274117675',
+        clientSecret: '215d9f623fe7750f702a535c943660ba',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
 };
