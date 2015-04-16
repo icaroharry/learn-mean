8bb293a32019fd82f26f18aa70b3fcb883738764
Ícaro Harry <icaropc17@gmail.com>
2015-04-16 00:33:45 -0300

    google oauth strategy

--- config/env/development.js
+++ config/env/development.js
@@ -5,5 +5,10 @@ module.exports = {
         clientID: '826355274117675',
         clientSecret: '215d9f623fe7750f702a535c943660ba',
         callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    },
+    google: {
+        clientID: '390717311094-foufr2d2j20cr9ronu570pjftona3qbo.apps.googleusercontent.com',
+        clientSecret: 'ctoP0OPjLCqSMQ25hnRX0oZc',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
     }
 };
